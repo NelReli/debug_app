21f87bcb73af726476a84be64a8a01e97e21474a
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2025-04-08 12:52:04 +0200

    suite

--- config/database.php
+++ config/database.php
@@ -8,8 +8,12 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connexion réussie";
+
+
     $stmt = $pdo->query("SELECT * FROM produits");
-$produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // recuperation des produits depuis la base de données
+$produit = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $produitsReindexés = [];
 foreach ($produits as $produit) {
     $produitsReindexés[$produit['id']] = $produit;
